e377fe3b0e01cd5af59163cb49eff9714f93a303
Helena <gomel-offis@mail.ru>
2025-03-08 17:11:44 +0300

    feat: add task 2

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -31,8 +31,20 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  let hours = new Date(date).getHours();
+  let minutes = new Date(date).getMinutes();
+  let seconds = new Date(date).getSeconds();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
