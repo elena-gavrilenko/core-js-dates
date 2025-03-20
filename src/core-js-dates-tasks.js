5a8d8f4d8418ca204854f4aaa42f6811c830a2b9
Helena <gomel-offis@mail.ru>
2025-03-20 23:47:59 +0300

    feat: add task 10

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -234,8 +234,17 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const d = new Date(date);
+  d.setDate(d.getDate() + 4 - (d.getDay() || 7));
+
+  const yearStart = new Date(d.getFullYear(), 0, 1);
+
+  const dayOfYear = Math.floor((d - yearStart) / (24 * 60 * 60 * 1000)) + 1;
+
+  const weekNumber = Math.ceil(dayOfYear / 7);
+
+  return weekNumber;
 }
 
 /**
