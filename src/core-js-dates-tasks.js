519287f4f4d8c37e6416f3e67a149e396dcf0661
Helena <gomel-offis@mail.ru>
2025-03-23 21:49:15 +0300

    feat: add task 11

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -258,8 +258,15 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentDate = new Date(date);
+
+  while (true) {
+    currentDate.setDate(currentDate.getDate() + 1);
+    if (currentDate.getDate() === 13 && currentDate.getDay() === 5) {
+      return currentDate;
+    }
+  }
 }
 
 /**
